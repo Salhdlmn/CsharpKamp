31d7f07c2d8c88c5f8f59281282a463e0e2dafb5
Salih Dilmen <80348831+Salhdlmn@users.noreply.github.com>
2024-10-27 23:40:01 +0300

    Add files via upload

---
+++ 06_Arrays/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Temel Dizi Örnekleri
+            //string[] colors = new string[2];
+            //colors[0] = "kırmızı";
+            //colors[1] = "Sarı";
+            //string[] cities = { "Prag", "Atina", "Ankara", "Bursa" };
+            //Console.WriteLine(cities[2]);
+
+            #endregion
+
+            #region Dizideki Tüm elemanları listeleme
+
+            //string[] colors = { "sarı", "kırmızı", "mavi", "yeşil", "turuncu" };
+            //for (int i = 0; i < colors.Length; i++) 
+            //{
+            // Console.WriteLine(colors[i]);
+            // };
+
+            //int[] numbers = { 4, 85, 96, 663, 123, 7822, 654, 1236, 4582 };
+
+            //for (int i = 0; i < numbers.Length; i++) 
+            //{
+            //    if (numbers[i] % 3 == 0) 
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+            //}
+
+            //int[] myArray = { 47, 85, 95, 41, 25, 635, 789, 86, 100 };
+            //int maxNumber=myArray[0];
+            //for (int i = 0; i < myArray.Length; i++) 
+            //{
+            //    if (myArray[i] > maxNumber)
+            //    {
+            //        maxNumber = myArray[i];
+            //    }
+            //}
+            //Console.WriteLine(maxNumber);
+
+
+
+            #endregion
+
+            #region Kullanıcıdan Değer Alma
+            //string[] cities = new string[5];
+
+            //for (int i = 0; i < cities.Length; i++)
+            //{
+            //    Console.Write($"Lütfen {i + 1}. Şehri Giriniz: ");
+            //    cities[i] = Console.ReadLine();
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("--------------");
+
+            //for (int i = 0; i < cities.Length; i++) 
+            //{
+            //    Console.WriteLine(cities[i]);
+            //}
+
+
+            #endregion
+
+
+
+            Console.Read();
+        }
+    }
+}
